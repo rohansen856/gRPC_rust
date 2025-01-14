abf44330284a36cf330ba7ab5f7c9db470a90f6f
rohansen856 <rohansen856@gmail.com>
2025-01-14 10:25:45 +0530

    client and server script done

---
+++ src/client.rs
@@ -0,0 +1,21 @@
+use greeter::greeter_client::GreeterClient;
+use greeter::HelloRequest;
+
+pub mod greeter {
+    tonic::include_proto!("greeter");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
---
+++ src/server.rs
@@ -0,0 +1,42 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use greeter::greeter_server::{Greeter, GreeterServer};
+use greeter::{HelloRequest, HelloReply};
+
+pub mod greeter {
+    tonic::include_proto!("greeter");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    println!("GreeterServer listening on {}", addr);
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
